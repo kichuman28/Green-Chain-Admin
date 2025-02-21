f1a5d043fd2cf2fdc357ffb3eceb903e24a1965b
Adwaith Jayasankar <kichu31084@gmail.com>
2025-02-21 21:33:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind CSS is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
